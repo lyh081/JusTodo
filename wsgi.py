906f63dbc90f811cf5260b8d89d304bf221ea691
lyh081 <lyhdut@163.com>
2019-04-07 14:53:00 +0800

    deploy
    ready for deploy

---
+++ wsgi.py
@@ -0,0 +1,10 @@
+from dotenv import load_dotenv
+import os
+
+dotenv_path = os.path.join(os.path.dirname(__file__), '.env')
+if os.path.exists(dotenv_path):
+    load_dotenv(dotenv_path)
+
+from JusTodo import create_app
+
+app = create_app('production')
